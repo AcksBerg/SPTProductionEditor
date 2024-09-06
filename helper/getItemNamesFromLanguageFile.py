fb4408ba3ca73dc2f37344964ac0d2a8962c39cd
AcksBerg <AcksBerg@users.noreply.github.com>
2024-09-06 14:15:27 +0200

    Removed Clothing and intruduced a blacklist for items which are unwanted but do not match the other filters

--- helper/getItemNamesFromLanguageFile.py
+++ helper/getItemNamesFromLanguageFile.py
@@ -6,10 +6,12 @@ with open("./helper/en.json", encoding="UTF8") as file:
 items = {}
 
 temp_items = {}
-
+id_blacklist = ["5447b5cf4bdc2d65278b4567", # Handgun
+                "6602bcf19cc643f44a04274b" # Stash
+                ]
 for key, value in data.items():
     id_part, attribute = key.split(" ", 1)
-    
+
     # Check if it is an Id
     if len(id_part) == 24 and id_part.isalnum():
         if id_part not in temp_items:
@@ -19,14 +21,19 @@ for key, value in data.items():
         break  # It is not an Id, all the items are in the front of the language file (hopefully)
 
 for id_part, attributes in temp_items.items():
+    # Remove Parentitems
+    # Remove more parentitems
+    # Remove stuff from blacklist (first two methods failed)
+    # Remove clothing
     if not (
-        attributes.get("shortname") == "Item" or 
+        attributes.get("shortname") == "Item" or
         attributes.get("description") == "Item"
     ) and (
         attributes.get("shortname") != attributes.get("description")
+    ) and (id_part not in id_blacklist) and not (
+        attributes.get("name").startswith("USEC ") or attributes.get("name").startswith("BEAR ")
     ):
         items[id_part] = attributes
-
 # Create the json output
 json_output = json.dumps(items, indent=4)
 # print(json_output) # for Debug
