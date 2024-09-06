1615492b40d605e6a4e9d83477039b4e26a0b29f
AcksBerg <AcksBerg@users.noreply.github.com>
2024-09-06 13:37:02 +0200

    Fixed an error with the keynames resulting in items which are not viable

--- helper/getItemNamesFromLanguageFile.py
+++ helper/getItemNamesFromLanguageFile.py
@@ -1,39 +1,33 @@
 import json
 
-# Eingabedaten
 with open("./helper/en.json", encoding="UTF8") as file:
     data = json.load(file)
 
-# Umwandlung in JSON-Format
 items = {}
 
-# Temporäre Speicherung von Einträgen, um sie später zu prüfen
 temp_items = {}
 
 for key, value in data.items():
-    # Trenne den Schlüssel in ID und Attribut
     id_part, attribute = key.split(" ", 1)
     
-    # Prüfe, ob die ID 24-stellig ist
+    # Check if it is an Id
     if len(id_part) == 24 and id_part.isalnum():
         if id_part not in temp_items:
             temp_items[id_part] = {}
         temp_items[id_part][attribute.lower()] = value
     else:
-        break  # Abbruch, wenn eine nicht passende ID gefunden wird
+        break  # It is not an Id, all the items are in the front of the language file (hopefully)
 
-# Filterung der IDs, die 'Item' als ShortName oder Description haben
 for id_part, attributes in temp_items.items():
     if not (
-        attributes.get("ShortName") == "Item" or 
-        attributes.get("Description") == "Item"
+        attributes.get("shortname") == "Item" or 
+        attributes.get("description") == "Item"
     ):
         items[id_part] = attributes
 
-# Ausgabe als JSON
+# Create the json output
 json_output = json.dumps(items, indent=4)
-# print(json_output)
+# print(json_output) # for Debug
 
-# Optional: Speichern in einer Datei
 with open("./src/data/item.json", "w") as json_file:
     json_file.write(json_output)
